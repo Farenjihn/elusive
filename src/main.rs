0a2c18f57a0361a47a6b5016dc2f302c53801739
Valentin Finini <valentin.finini@cysec.systems>
2019-11-19 14:26:56 +0100

    Add clap, a bit of cleanup

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,42 @@
-mod builder;
 mod config;
+mod initramfs;
 
-use builder::Builder;
 use config::Config;
 
+use clap::{App, AppSettings, Arg, SubCommand};
 use std::error::Error;
 use std::fs;
 
+const CONFIG_PATH: &str = "/etc/elusive.toml";
+
 fn main() -> Result<(), Box<dyn Error>> {
-    let data = fs::read("example.toml")?;
+    let matches = App::new("elusive")
+        .setting(AppSettings::SubcommandRequiredElseHelp)
+        .arg(
+            Arg::with_name("config")
+                .short("c")
+                .long("config")
+                .takes_value(true)
+                .global(true)
+                .help("Path to the configuration file"),
+        )
+        .subcommand(
+            SubCommand::with_name("initramfs")
+                .about("Generate a compressed cpio archive for initramfs"),
+        )
+        .get_matches();
+
+    let config_path = matches.value_of("config").unwrap_or_else(|| CONFIG_PATH);
+    let data = fs::read(config_path)?;
     let config: Config = toml::from_slice(&data)?;
 
-    let builder = Builder::from_config(config)?;
-    builder.build()?;
+    match matches.subcommand() {
+        ("initramfs", Some(_)) => {
+            let builder = initramfs::Builder::from_config(config.initramfs)?;
+            builder.build()?;
+        }
+        (subcommand, _) => unreachable!("unknown subcommand {}", subcommand),
+    }
 
     Ok(())
 }
